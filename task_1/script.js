e1181f3a92eae6ffd9ac3b52d1e197c20e9f83d8
Pavel Kulyasov <pavel.kulyasov@gmail.com>
2023-03-22 00:13:32 +0200

    fix timer

--- task_1/script.js
+++ task_1/script.js
@@ -13,15 +13,19 @@ class Timer {
    }
 
    #stopTimer() {
+      root.style.setProperty('color', 'rgba(228,229,234,0.56)')
       clearInterval(this.#timerInstance)
       this.#timerInstance = null
    }
 
    app() {
       if (this.#timerInstance) return this.#stopTimer()
+      root.style.setProperty('color', 'rgba(228,229,234,0.86)')
       this.#timerInstance = setInterval(() => this.root.textContent = this.#getDate(), 1000)
    }
 }
 
-const timer = new Timer(root).app()
+const timer = new Timer(root)
+timer.app()
+
 root.onclick = () => timer.app()
