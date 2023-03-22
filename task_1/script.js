221327ce351737f8d978525ef6e39dd871b606e9
Pavel Kulyasov <pavel.kulyasov@gmail.com>
2023-03-22 09:32:16 +0200

    fixed wrong task 1

--- task_1/script.js
+++ task_1/script.js
@@ -1,31 +1,38 @@
-const root = document.querySelector('#root')
-root.textContent = new Date().toLocaleTimeString()
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+const timerEl = document.querySelector("span");
 
-class Timer {
-   #timerInstance
+const createTimerAnimator = () => {
+  return (seconds) => {
+    let remainingTime = seconds * 1000;
+    const timerInterval = setInterval(() => {
+      if (!remainingTime) {
+        clearInterval(timerInterval);
+        timerEl.textContent = "hh:mm:ss";
+        return;
+      }
+      remainingTime = remainingTime - 1000;
+      let seconds = Math.floor((remainingTime / 1000) % 60).toString();
+      let minutes = Math.floor((remainingTime / (1000 * 60)) % 60).toString();
+      let hours = Math.floor(
+        (remainingTime / (1000 * 60 * 60)) % 24
+      ).toString();
+      timerEl.textContent = `${hours.padStart(2, "0")}:${minutes.padStart(
+        2,
+        "0"
+      )}:${seconds.padStart(2, "0")}`;
+    }, 1000);
+  };
+};
 
-   constructor(root) {
-      this.root = root
-   }
+const animateTimer = createTimerAnimator();
 
-   #getDate() {
-      return  new Date().toLocaleTimeString()
-   }
+inputEl.addEventListener("input", (e) => {
+  if (isNaN(e.target.value)) e.target.value = e.target.value.slice(0, -1);
+});
 
-   #stopTimer() {
-      root.style.setProperty('color', 'rgba(228,229,234,0.56)')
-      clearInterval(this.#timerInstance)
-      this.#timerInstance = null
-   }
-
-   app() {
-      if (this.#timerInstance) return this.#stopTimer()
-      root.style.setProperty('color', 'rgba(228,229,234,0.86)')
-      this.#timerInstance = setInterval(() => this.root.textContent = this.#getDate(), 1000)
-   }
-}
-
-const timer = new Timer(root)
-timer.app()
-
-root.onclick = () => timer.app()
+buttonEl.addEventListener("click", () => {
+  const seconds = Number(inputEl.value);
+  animateTimer(seconds);
+  inputEl.value = "";
+});
